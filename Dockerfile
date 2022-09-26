da72a06a054a341a4988ade70fa5ca945fa93cf4
Shaked123456 <shaked@wizards.co.il>
2022-09-26 17:04:20 +0300

    added all files

---
+++ Dockerfile
@@ -0,0 +1,9 @@
+FROM ubuntu:latest
+ENV VERSION=1.2.0
+RUN apt update && apt install --no-install-recommends --assume-yes \
+ 	python3 \
+        vim \
+	zip \
+	unzip
+COPY zip_job.py /tmp
+ENTRYPOINT ["./InitialScript.sh"]	
---
+++ zip_job.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+
+from zipfile import Zipfile
+
+array = ['a', 'b', 'c', 'd']
+VERSION = '1.2.0'
+
+
+for arr in array:
+	open('{}.txt'.format(arr), 'w')
+
+for arr in array:
+	try:
+        open('{}.txt'.format(arr), 'r'):
+	except IndexError:
+        print("ERROR: something went wrong")
+
+for arr in array:
+    with ZipFile('{}_{}.zip'.format(arr, VERSION), 'w') as zipf:
+        zipf.write('{}.txt'.format(arr))
+
+for arr in array:
+	try:
+        open('{}_{}.zip'.format(arr, VERSION), 'r'):
+	except IndexError:
+        print("ERROR: something went wrong")
