f51a5506e093128548d24be17d1fb0b8aab804ea
shaked123456 <97021896+shaked123456@users.noreply.github.com>
2022-09-26 17:47:24 +0300

    Update zip_job.py

--- zip_job.py
+++ zip_job.py
@@ -7,20 +7,20 @@ VERSION = '1.2.0'
 
 
 for arr in array:
-	open('{}.txt'.format(arr), 'w')
+    open('{}.txt'.format(arr), 'w')
 
 for arr in array:
-	try:
-        open('{}.txt'.format(arr), 'r'):
-	except IndexError:
-        print("ERROR: something went wrong")
+    try:
+    open('{}.txt'.format(arr), 'r'):
+    except IndexError:
+    print("ERROR: something went wrong")
 
 for arr in array:
     with ZipFile('{}_{}.zip'.format(arr, VERSION), 'w') as zipf:
         zipf.write('{}.txt'.format(arr))
 
 for arr in array:
-	try:
-        open('{}_{}.zip'.format(arr, VERSION), 'r'):
-	except IndexError:
-        print("ERROR: something went wrong")
+    try:
+    open('{}_{}.zip'.format(arr, VERSION), 'r'):
+    except IndexError:
+    print("ERROR: something went wrong")
