21a50b3aea2521645db6107698a2c870907368e9
shaked123456 <97021896+shaked123456@users.noreply.github.com>
2022-10-02 13:52:56 +0300

    Update zip_job.py

--- zip_job.py
+++ zip_job.py
@@ -10,13 +10,7 @@ VERSION = os.environ['VERSION']
 
 for arr in array:
     open('{}.txt'.format(arr), 'w')
-
-for arr in array:
-     open('{}.txt'.format(arr), 'r')
-
-for arr in array:
+    open('{}.txt'.format(arr), 'r')
     with ZipFile('{}_{}.zip'.format(arr, VERSION), 'w') as zipf:
         zipf.write('{}.txt'.format(arr))
-
-for arr in array:
-     open('{}_{}.zip'.format(arr, VERSION), 'r')
+    open('{}_{}.zip'.format(arr, VERSION), 'r')
